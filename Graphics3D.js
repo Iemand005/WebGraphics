723f4afbbb3e635b84a699258ded90530be4d58f
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-18 01:44:38 +0200

    safety

--- Graphics3D.js
+++ Graphics3D.js
@@ -322,11 +322,10 @@ Graphics3D.prototype.presentRenderTarget = function () {
  * @param {string} source 
  * @returns {WebGLShader?}
  */
-var hi;
-
 Graphics3D.prototype.loadShader = function (type, source) {
 	if (!this.gl) return null;
   const shader = this.gl.createShader(type);
+  if (!shader) return null;
   this.gl.shaderSource(shader, source);
   this.gl.compileShader(shader);
 
